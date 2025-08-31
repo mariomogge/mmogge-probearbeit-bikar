b85dacd166546c55d81126497002df31f0c4c2d1
Mario Mogge <mario.mogge@gmx.de>
2025-08-31 15:32:51 +0200

    feat: add account service

---
+++ src/Application/Service/AccountService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Application\Service;
+
+use App\Domain\Account\{Account, AccountServiceInterface, Money, Transaction, TransactionType};
+use App\Domain\User\User;
+use App\Infrastructure\Repository\{AccountRepository, TransactionRepository};
+use Doctrine\ORM\EntityManagerInterface;
+
+final class AccountService implements AccountServiceInterface
+{
+    public function __construct(
+        private readonly AccountRepository $accounts,
+        private readonly TransactionRepository $transactions,
+        private readonly EntityManagerInterface $em,
+    ) {}
+
+    public function openAccount(User $owner, ?Money $initialDeposit = null): Account
+    {
+        $account = new Account($owner);
+        $this->em->persist($account);
+        if ($initialDeposit && $initialDeposit->cents > 0) {
+            $this->deposit($account, $initialDeposit);
+        }
+        $this->em->flush();
+        return $account;
+    }
+
+    public function deposit(Account $account, Money $amount): Transaction
+    {
+        $account->deposit($amount);
+        $tx = new Transaction($account, TransactionType::DEPOSIT, $amount, $account->getBalance());
+        $this->em->persist($tx);
+        $this->em->flush();
+        return $tx;
+    }
+
+    public function withdraw(Account $account, Money $amount): Transaction
+    {
+        $account->withdraw($amount); // may throw DomainException
+        $tx = new Transaction($account, TransactionType::WITHDRAW, $amount, $account->getBalance());
+        $this->em->persist($tx);
+        $this->em->flush();
+        return $tx;
+    }
+}
